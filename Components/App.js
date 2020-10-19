5eb0297e2a589dca3187cee071e6b3692782c5c0
AfraGngr <gungorafra93@gmail.com>
2020-10-19 22:46:23 +0300

    App.js file  was initialized

---
+++ Components/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import {SafeAreaView, View, Text, StyleSheet} from "react-native";
+
+
+const App = () => {
+  return (
+    <SafeAreaView style = {{flex : 1}}>
+      <View style={styles.container}>
+        <Text style = {[styles.text, {color: "rgb(170, 71,126)"}]}>Clarusway</Text>
+        {/* styles.text yanına styles.newDesign gibi bir şey eklersem en son yazanı override eder. */}
+        <Text style = {styles.text}>Components</Text>
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create ({
+  container :{
+    flex : 1,
+    justifyContent :"center",
+    alignItems: "center",
+  },
+
+  text : {
+    fontSize:40,
+    fontWeight: "bold"
+  }
+})
