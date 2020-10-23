5c49cc59d817e3be99dbf2cdf4387c3edf155bc7
AfraGngr <gungorafra93@gmail.com>
2020-10-24 02:33:36 +0300

    Social Media project is created.

--- SocialMediaProject/index.js
+++ SocialMediaProject/index.js
@@ -1,9 +1,9 @@
 /**
  * @format
  */
-import 'react-native-gesture-handler';
+
 import {AppRegistry} from 'react-native';
-import Router from './src_2/Router';
+import Router from './src/Router';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => Router);
