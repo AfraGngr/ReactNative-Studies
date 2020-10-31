b5d80f3f68b95327d0ba73964ed4c4cd95165b72
AfraGngr <gungorafra93@gmail.com>
2020-11-01 00:29:59 +0300

    Project completed.

--- Redux/src/context/reducer.js
+++ Redux/src/context/reducer.js
@@ -1,10 +1,16 @@
-export function reducer (state, action){
-    switch(action.type){
-        case "ADD_TO_FAVORİTE":
-            const newList = [...state.favoriteList]
-            newList.push(action.payload.selectedRestaurant)
-            state.favoriteList = newList
-            return { ...state }
+export function reducer(state, action) {
+    switch (action.type) {
+        case "ADD_TO_FAVORITE":
+            // const newList = [...state.favoriteList];
+            // newList.push(action.payload.selectedRestaurant);
+            // state.favoriteList = newList;
+            // return { ...state }
+
+            return {
+                ...state,
+                favoriteList: [...state.favoriteList, action.payload.selectedRestaurant]
+            }
+
         default:
             return state;
     }
