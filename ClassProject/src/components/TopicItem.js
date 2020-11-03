86362b623138b80e72eb4d193ce5d83e7884feb3
AfraGngr <gungorafra93@gmail.com>
2020-11-04 00:27:14 +0300

    Project is finished

--- ClassProject/src/components/TopicItem.js
+++ ClassProject/src/components/TopicItem.js
@@ -1,10 +1,17 @@
-import React from "react";
-import { TouchableOpacity, Text } from "react-native";
+import React from 'react';
+import {TouchableOpacity, Text} from 'react-native';
 
-const TopicItem = props => {
-    return (
-        null 
-    )
-}
+import { topicItem } from '../styles';
 
-export { TopicItem };
+const TopicItem = (props) => {
+  return (
+      <TouchableOpacity
+        style={[topicItem.container, { backgroundColor: `#${props.item.color}` }]}
+        onPress={props.onSelect}
+      >
+          <Text style={topicItem.text}>{props.item.name}</Text>
+      </TouchableOpacity>
+    );
+};
+
+export {TopicItem};
